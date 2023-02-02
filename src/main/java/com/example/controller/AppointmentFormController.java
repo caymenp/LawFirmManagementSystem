5bdf59462f709aab55a92d86ba2b57b92a07b9dc
caymenp <109010237+caymenp@users.noreply.github.com>
2023-02-01 20:54:29 -0600

    Fixed issue causing existing appointments not being able to be rescheduled due to scheduling overlap.

--- src/main/java/com/example/controller/AppointmentFormController.java
+++ src/main/java/com/example/controller/AppointmentFormController.java
@@ -60,12 +60,14 @@ public class AppointmentFormController implements Initializable {
     Appointment passApt;
     User passUser;
 
-    /** Gets data passed from the AppointmentOverviewForm.
+    /**
+     * Gets data passed from the AppointmentOverviewForm.
      * When the scene changes from the main stage (AppointmentOverview) from am action event, this method gets all the
      * data (objects) passed to it, so it can reference this data in the new scene. Used to grab the info from an existing
      * appointment and display the existing data in the fields.
+     *
      * @param passedApt Appointment that was selected on the appointment table when "Modify Appointment" was clicked
-     * @param user Current logged in user to ensure logging of updates are handled correctly.
+     * @param user      Current logged in user to ensure logging of updates are handled correctly.
      */
     public void getData(Appointment passedApt, User user) {
         passApt = passedApt;
@@ -99,10 +101,12 @@ public class AppointmentFormController implements Initializable {
     }
 
 
-    /** Getting Data from mainStage when "Add Appointment" is clicked.
+    /**
+     * Getting Data from mainStage when "Add Appointment" is clicked.
      * This is a overridden method to allow the reuse of the same appointment form whether "Modify Appointment" or "Add
-     *  Appointment" is clicked. This method allows the passing of JUST the current user, since there is no appointment
-     *   to pass when a new appointment is being made.
+     * Appointment" is clicked. This method allows the passing of JUST the current user, since there is no appointment
+     * to pass when a new appointment is being made.
+     *
      * @param user Current logged in user. To ensure accurate reporting on updates
      */
     public void getData(User user) {
@@ -110,13 +114,14 @@ public class AppointmentFormController implements Initializable {
     }
 
 
-
     LocalDate selectedStartDate;
     LocalDate selectedEndDate;
 
-    /**Gets value of StartDate date picker field.
+    /**
+     * Gets value of StartDate date picker field.
      * Sets the value of datepicker field to a local variable @selectedStartDate to be used by other methods
      * Then sets the endDate picker as enabled (validation), and pre-sets the date as the same day (but can be changed).
+     *
      * @param actionEvent triggered when the date picker is clicked and a date is selected.
      */
     public void startDate(ActionEvent actionEvent) {
@@ -125,8 +130,10 @@ public class AppointmentFormController implements Initializable {
         endDate.setValue(selectedStartDate.plusDays(0));
     }
 
-    /** Gets value of EndDate from datepicker
+    /**
+     * Gets value of EndDate from datepicker
      * Sets the selected endDate to a local variable for use by other methods
+     *
      * @param actionEvent triggered when the date picker is clicked and a date is selected.
      */
     public void endDate(ActionEvent actionEvent) {
@@ -135,9 +142,11 @@ public class AppointmentFormController implements Initializable {
 
     User selectedUser;
 
-    /**Gets selected User ID form the userID comboBox
+    /**
+     * Gets selected User ID form the userID comboBox
      * Gets the value of the comboBox and uses it as an argument of a method on the Users class that returns the
      * referenced user object. Then assigns this user object to a local variable to be used by other methods.
+     *
      * @param actionEvent
      */
     public void userIDList(ActionEvent actionEvent) {
@@ -147,9 +156,11 @@ public class AppointmentFormController implements Initializable {
 
     LocalTime startTimeSelected;
 
-    /**Start Time Selected
+    /**
+     * Start Time Selected
      * Gets the start time value from the comboBox, and sets end time to be 1 hour ahead of selected start time and
      * sets the end time combobox as visibile.
+     *
      * @param actionEvent
      */
     public void startTimeList(ActionEvent actionEvent) {
@@ -159,8 +170,10 @@ public class AppointmentFormController implements Initializable {
         endTimeList.setValue(startTimeSelected.plusHours(1));
     }
 
-    /**Cancel Button on the Appointment Form
+    /**
+     * Cancel Button on the Appointment Form
      * Closes out of the pop-up window, and sets the stage back to the main stage
+     *
      * @param actionEvent cancel button on the form
      */
     public void cancelBTN(ActionEvent actionEvent) {
@@ -168,10 +181,12 @@ public class AppointmentFormController implements Initializable {
         stage.close();
     }
 
-    /** Appointment Form Handler When Saved
+    /**
+     * Appointment Form Handler When Saved
      * Validates the form to ensure no empty text fields or comboBoxes. Also validates the appoitnment dates/times to
      * reduce error. If validation passes, it determines is this is a modification or addition by checking the form label
      * text value. Then submits the form data to the Appointment handler to be updated/added to the DB.
+     *
      * @param actionEvent
      */
     public void saveNewAppointment(ActionEvent actionEvent) {
@@ -239,132 +254,140 @@ public class AppointmentFormController implements Initializable {
         Contact contact = Contact.getContactByName(contactList.getValue());
         User user = User.getUser(userIDList.getValue());
 
-            String title = appointmentTitle.getText();
-            String description = appointmentDescription.getText();
-            String location = appointmentLocation.getText();
-            String type = appointmentType.getText();
-            Timestamp startDateTime = Timestamp.valueOf(LocalDateTime.of(startDate.getValue(), startTimeList.getValue()));
-            Timestamp endDateTime = Timestamp.valueOf(LocalDateTime.of(endDate.getValue(), endTimeList.getValue()));
+        String title = appointmentTitle.getText();
+        String description = appointmentDescription.getText();
+        String location = appointmentLocation.getText();
+        String type = appointmentType.getText();
+        Timestamp startDateTime = Timestamp.valueOf(LocalDateTime.of(startDate.getValue(), startTimeList.getValue()));
+        Timestamp endDateTime = Timestamp.valueOf(LocalDateTime.of(endDate.getValue(), endTimeList.getValue()));
 
-            Timestamp createdDate = Timestamp.valueOf(LocalDateTime.now());
-            String createdBy = passUser.getUserName();
+        Timestamp createdDate = Timestamp.valueOf(LocalDateTime.now());
+        String createdBy = passUser.getUserName();
 
-            if (appointmentFormLabel.getText().equals("Modify Appointment")) {
-                createdDate = passApt.getCreateDateTime();
-                createdBy = passApt.getCreatedBy();
-            }
+        if (appointmentFormLabel.getText().equals("Modify Appointment")) {
+            createdDate = passApt.getCreateDateTime();
+            createdBy = passApt.getCreatedBy();
+        }
 
-            Timestamp lastUpdated = Timestamp.valueOf(LocalDateTime.now());
-            String lastUpdatedBy = passUser.getUserName();
-            int customerID = customer.getCustomerID();
-            int userID = User.getUser(userIDList.getValue()).getUserID();
-            int contactID = contact.getContactID();
-
-
+        Timestamp lastUpdated = Timestamp.valueOf(LocalDateTime.now());
+        String lastUpdatedBy = passUser.getUserName();
+        int customerID = customer.getCustomerID();
+        int userID = User.getUser(userIDList.getValue()).getUserID();
+        int contactID = contact.getContactID();
 
 
-            if (appointmentFormLabel.getText().equals("Add Appointment")) {
-                Appointment appointment = new Appointment(title, description, location,
-                        type, startDateTime, endDateTime, createdDate, createdBy,
-                        lastUpdated, lastUpdatedBy, customerID, userID, contactID);
+        if (appointmentFormLabel.getText().equals("Add Appointment")) {
+            Appointment appointment = new Appointment(title, description, location,
+                    type, startDateTime, endDateTime, createdDate, createdBy,
+                    lastUpdated, lastUpdatedBy, customerID, userID, contactID);
 
-                if (!checkAppointmentOverlap(customerID, appointment)) {
-                    Appointment.addNewAppointment(appointment);
-                } else {
-                    AlertMessages alertMessages = new AlertMessages();
-                    alertMessages.errorMessage("Scheduling Interference", "Customer has an existing appointment between your selected date and times.");
-                    return;
-                }
-
+            if (checkAppointmentOverlap(customerID, appointment)) {
+                Appointment.addNewAppointment(appointment);
             } else {
-                int oldAptID = passApt.getAppointmentID();
-
-                Appointment appointment = new Appointment(oldAptID, title, description, location,
-                        type, startDateTime, endDateTime, createdDate, createdBy,
-                        lastUpdated, lastUpdatedBy, customerID, userID, contactID);
-
-                if (!checkAppointmentOverlap(customerID, appointment)) {
-                    Appointment.updateAppointment(appointment);
-                }else {
-                    AlertMessages alertMessages = new AlertMessages();
-                    alertMessages.errorMessage("Scheduling Interference", "Customer has an existing appointment between your selected date and times.");
-                    return;
-                }
-
+                AlertMessages alertMessages = new AlertMessages();
+                alertMessages.errorMessage("Scheduling Interference", "Customer has an existing appointment between your selected date and times.");
+                return;
             }
 
+        } else {
+            int oldAptID = passApt.getAppointmentID();
 
+            Appointment appointment = new Appointment(oldAptID, title, description, location,
+                    type, startDateTime, endDateTime, createdDate, createdBy,
+                    lastUpdated, lastUpdatedBy, customerID, userID, contactID);
+
+            if (checkAppointmentOverlap(customerID, appointment)) {
+                Appointment.updateAppointment(appointment);
+            } else {
+                AlertMessages alertMessages = new AlertMessages();
+                alertMessages.errorMessage("Scheduling Interference", "Customer has an existing appointment between your selected date and times.");
+                return;
+            }
+
+        }
 
 
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
     }
 
-    /**Checks for overlapping appointments on customers.
+
+
+    /**
+     * Checks for overlapping appointments on customers.
      * Checks for existing appointments by the customer, and ensures that the customer doesnt have an overlap.
+     *
      * @param cxID
      * @param cxAP
      * @return
      */
+    public static ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+
     public static boolean checkAppointmentOverlap(int cxID, Appointment cxAP) {
-        ObservableList<Appointment> allCustomerAppointments = FXCollections.observableArrayList();
+        boolean flag = true;
 
-        allCustomerAppointments.addAll(Appointment.customerAppointments(cxID));
+        allAppointments.addAll(Appointment.customerAppointments(cxID));
+        LocalDateTime newAppointmentStart = cxAP.getStartDateTime().toLocalDateTime();
+        LocalDateTime newAppointmentEnd = cxAP.getEndDateTime().toLocalDateTime();
 
-        LocalDateTime newApStart = cxAP.getStartDateTime().toLocalDateTime();
-        LocalDateTime newApEnd = cxAP.getEndDateTime().toLocalDateTime();
-
-        for (Appointment ap : allCustomerAppointments) {
-            LocalDateTime apStart = ap.getStartDateTime().toLocalDateTime();
-            LocalDateTime apEnd = ap.getEndDateTime().toLocalDateTime();
-
-            if (ap.getAppointmentID() == cxAP.getAppointmentID()) {
-                if (apStart.isEqual(newApStart) && apEnd.isEqual(newApEnd)) {
-                    continue;
-                }
+        for (Appointment a : allAppointments) {
+            if (a.getCustomerID() != cxID || a.getAppointmentID() == cxAP.getAppointmentID()) {
+                continue;
             }
 
-            if (newApStart.toLocalDate().equals(apStart.toLocalDate())) {
-                    if ((newApStart.isAfter(apStart) && newApEnd.isBefore(apEnd)) || (newApStart.isBefore(apStart) && newApEnd.isAfter(apEnd))) {
-                        return true;
-                    } else if (newApStart.toLocalTime() == apStart.toLocalTime() || newApEnd.toLocalTime() == apEnd.toLocalTime()) {
-                        return true;
-                    } else if ((apStart.isAfter(newApStart) && apStart.isBefore(newApEnd)) || (apStart.isBefore(newApStart) && apEnd.isAfter(newApEnd))) {
-                        return true;
-                    }
+            LocalDateTime existingStart = a.getStartDateTime().toLocalDateTime();
+            LocalDateTime existingEnd = a.getEndDateTime().toLocalDateTime();
+
+            if (newAppointmentStart.isEqual(existingStart) || newAppointmentEnd.isEqual(existingEnd)) {
+                flag = false;
+            } else if (newAppointmentStart.isBefore(existingStart) && newAppointmentEnd.isAfter(existingStart)) {
+                flag = false;
+            } else if (newAppointmentStart.isBefore(existingEnd) && newAppointmentEnd.isAfter(existingEnd)) {
+                flag = false;
+            } else if (newAppointmentStart.isBefore(existingStart) && newAppointmentEnd.isAfter(existingEnd)) {
+                flag = false;
+            } else if (newAppointmentStart.isAfter(existingStart) && newAppointmentEnd.isBefore(existingEnd)) {
+                flag = false;
             }
+
+            if (!flag) {
+                break;
+            }
+
         }
-        return false;
+
+        return flag;
     }
 
-    private void initializeContacts() {
-        contactList.setItems(Contact.getAllContactStrings());
-    }
+        private void initializeContacts () {
+            contactList.setItems(Contact.getAllContactStrings());
+        }
 
-    private void initializeCustomers() {
-        customerList.setItems(Customer.getAllCustomerStrings());
-    }
+        private void initializeCustomers () {
+            customerList.setItems(Customer.getAllCustomerStrings());
+        }
 
-    private void initializeUsers() {
-        userIDList.setItems(User.getAllUserStrings());
-    }
+        private void initializeUsers () {
+            userIDList.setItems(User.getAllUserStrings());
+        }
 
-    private void initializeTimeBox() {
-        startTimeList.setItems(DateTimeConversion.availableAppointmentTimes());
-        endTimeList.setItems(DateTimeConversion.availableAppointmentTimes());
-    }
+        private void initializeTimeBox () {
+            startTimeList.setItems(DateTimeConversion.availableAppointmentTimes());
+            endTimeList.setItems(DateTimeConversion.availableAppointmentTimes());
+        }
 
-    /**Initializes comboBoxes on load
-     *
-     * @param url
-     * @param resourceBundle
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        initializeTimeBox();
-        initializeUsers();
-        initializeCustomers();
-        initializeContacts();
-    }
+        /**Initializes comboBoxes on load
+         *
+         * @param url
+         * @param resourceBundle
+         */
+        @Override
+        public void initialize (URL url, ResourceBundle resourceBundle){
+            initializeTimeBox();
+            initializeUsers();
+            initializeCustomers();
+            initializeContacts();
+        }
+
 
 }
